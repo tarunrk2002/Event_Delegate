fb0f7e08f6637c7826c01f01cb7072e59ae143c9
tarun <tarunrk2002@gmail.com>
2024-06-21 16:49:04 +0530

    progress for delegates and events

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace events_delegates
     {
         static void Main(string[] args)
         {
+            vid vidd = new vid() { Title = "hi" };
+            vid vidd2 = new vid() { Title = "hello" };
+
+            encoder encoder = new encoder();
+            encoder.enco(vidd);
         }
     }
 }
---
+++ encoder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace events_delegates
+{
+    internal class encoder
+    {
+        public void enco(vid vid) 
+        {
+            Console.WriteLine("encodingg...!!!");
+            Thread.Sleep(4000);
+        }
+    }
+}
